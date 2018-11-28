1eb7bb47aff70e68d2dff39e37076c2244f33dee
Moix1 <abdulmoix1@outlook.com>
2018-11-28 19:27:36 +0500

    Intial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abdul\desktop\pythonprojects\btre_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
